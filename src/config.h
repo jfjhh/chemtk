24f3ad142f84c45ffe535a3bb182b1ebea40badb
Alex Striff <alex.striff1@gmail.com>
2015-12-31 03:52:54 -0800

    Fix: Travis now runs make distcheck

---
+++ src/config.h
@@ -0,0 +1,16 @@
+/**
+ * @file config.h
+ * @author Alex Striff
+ *
+ * @brief Holds configuration values.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**
+ * Dummy file, for now.
+ * */
+
+#endif /* CONFIG_H */
+
